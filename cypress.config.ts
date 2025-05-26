959af3a4cee4243135aaa48adeebc6bc3ea0d99b
Freek Boes <r0984517@student.thomasmore.be>
2025-05-26 23:59:47 +0200

    workflow

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "s1qa15",    // ← Verplaats projectId naar hier (root of global config)
+
   e2e: {
+    // baseUrl, spec patterns, etc kun je hier ook zetten
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      
-        projectId: "s1qa15"
+      // implement node event listeners hier
+      return config;
     },
   },
 });
